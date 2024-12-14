f0e4a8b52b63cfd64399fcce75d27cd3469acd5e
Eros Coelho <eros.limacoelho@student.griffith.ie>
2024-12-14 16:14:52 +0000

    constructed the Person class and overrode toString and compareTo methods

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,35 @@
-class Person {
+import java.time.LocalDate;
 
+public final class Person implements Comparable<Person> {
+    private final String surname;
+    private final String firstname;
+    private final LocalDate dateOfBirth;
 
+    public Person(String firstname, String surname, LocalDate dateOfBirth) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return firstname + " " + surname + " " + dateOfBirth;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return this.firstname.compareTo(other.firstname);
+    }
 }
